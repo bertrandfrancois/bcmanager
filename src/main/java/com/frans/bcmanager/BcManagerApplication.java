a7008ab5f687969c3bcd3b65660c165ae4e555df
francois.bertrand <francois.bertrand@famifed.be>
2020-03-03 18:55:34 +0100

    initial commit

---
+++ src/main/java/com/frans/bcmanager/BcManagerApplication.java
@@ -0,0 +1,13 @@
+package com.frans.bcmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frans/bcmanager/BcManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frans.bcmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
